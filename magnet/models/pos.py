90b5dae958496c73adaeea076b44dfd011a3077a
Krisztian Eyssen <krisz@eyssen.hu>
2020-02-06 11:37:37 +0100

    Update pos.py

--- magnet/models/pos.py
+++ magnet/models/pos.py
@@ -47,7 +47,7 @@ class PosOrder(models.Model):
     _inherit = 'pos.order'
     
     
-    def create_from_app(self, pos_id):
+    def create_from_app(self, pos_id, product_id, img):
 
         Session = self.env['pos.session'].search([('config_id', '=', pos_id), ('state', '=', 'opened')])
         if Session:
@@ -57,55 +57,51 @@ class PosOrder(models.Model):
                 sequence_number = Order.sequence_number + 1
             else:
                 sequence_number = 1
-            
-            vals = {
-                'session_id': Session.id,
-                'state': 'draft',
-                'to_invoice': False,
-                'sequence_number': sequence_number,
-                'name': self.env['ir.sequence'].browse(Session.config_id.sequence_id.id)._next(),
-                'company_id': Session.company_id.id,
-                'pricelist_id': 1,
-                'nb_print': 0,
-                'pos_reference': 'Order ' + format(Session.id, '05d') + '-' + format(pos_id, '03d') + '-' + format(sequence_number, '04d'), #TODO: ezt lehet hogy máshogy kéne számolni
-                'amount_tax': 0,
-                'amount_total': 0,
-                'amount_paid': 0,
-                'amount_return': 0
-            }
-            Order = self.env['pos.order'].create(vals)
-            return Order.id
 
-        else:
-            return 0
-    
-    
-    def add_order_line(self, order_id, product_id, img):
-        
-        Order = self.env['pos.order'].search([('id', '=', order_id)], limit=1)
-        Product = self.env['product.product'].search([('id', '=', product_id)], limit=1)
-        if Order and Product:
+            Product = self.env['product.product'].search([('id', '=', product_id)], limit=1)
             
-            vals = {
-                'company_id': Order.company_id.id,
-                'name': self.env['ir.sequence'].browse(Order.session_id.config_id.sequence_line_id.id)._next(),
-                'product_id': Product.id,
-                'qty': 1,
-                'price_unit': Product.list_price,
-                'price_subtotal': Product.list_price,
-                'price_subtotal_incl': Product.list_price * 1.27, #TODO: Ide bruttó ár kell!
-                'order_id': Order.id,
-                'discount': 0,
-                'print_image': img
-            }
-            self.env['pos.order.line'].create(vals)
+            if Order and Product:
+
+                vals = {
+                    'session_id': Session.id,
+                    'state': 'draft',
+                    'to_invoice': False,
+                    'sequence_number': sequence_number,
+                    'name': self.env['ir.sequence'].browse(Session.config_id.sequence_id.id)._next(),
+                    'company_id': Session.company_id.id,
+                    'pricelist_id': 1,
+                    'nb_print': 0,
+                    'pos_reference': 'Order ' + format(Session.id, '05d') + '-' + format(pos_id, '03d') + '-' + format(sequence_number, '04d'), #TODO: ezt lehet hogy máshogy kéne számolni
+                    'amount_tax': 0,
+                    'amount_total': 0,
+                    'amount_paid': 0,
+                    'amount_return': 0
+                }
+                Order = self.env['pos.order'].create(vals)
+
+                vals = {
+                    'company_id': Order.company_id.id,
+                    'name': self.env['ir.sequence'].browse(Order.session_id.config_id.sequence_line_id.id)._next(),
+                    'product_id': Product.id,
+                    'qty': 1,
+                    'price_unit': Product.list_price,
+                    'price_subtotal': Product.list_price,
+                    'price_subtotal_incl': Product.list_price * 1.27, #TODO: Ide bruttó ár kell!
+                    'order_id': Order.id,
+                    'discount': 0,
+                    'print_image': img
+                }
+                self.env['pos.order.line'].create(vals)
+                
+                Order.amount_tax = Product.list_price * 0.27 #TODO: Ide az ÁFA kell
+                Order.amount_total = Product.list_price * 1.27
+                Order.amount_paid = Product.list_price * 1.27 #TODO: Ezt majd a fizetéshez kell állítani
+                
+                return Order.id
             
-            Order.amount_tax = Product.list_price * 0.27 #TODO: Ide az ÁFA kell
-            Order.amount_total = Product.list_price * 1.27
-            Order.amount_paid = Product.list_price * 1.27 #TODO: Ezt majd a fizetéshez kell állítani
-            
-            return 1
-            
+            else:
+                return 0
+
         else:
             return 0
 
